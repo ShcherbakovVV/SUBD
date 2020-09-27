fe36c8f48cc0d103a1dfe84e9c690164280d1a4d
V.Shcherbakov <vitya.shcherbakov@gmail.com>
2020-09-27 19:07:54 +0400

    Added GUI Python script

---
+++ main.py
@@ -0,0 +1,79 @@
+import mysql.connector
+from tkinter import *
+
+#вместо 0000 указать свой пароль, указанный при установке MySQl
+db_connect = mysql.connector.connect( host = 'localhost',
+                                      database = 'fut_cen_bum',
+                                      user = 'root',
+                                      password = '0000' )
+
+root=Tk()                    #создание окна
+
+root.geometry('1000x600')    #размер окна
+root.title('Фьючерсы')       #заголовок окна
+
+#надпись "Фильтры"
+filters = Label( root, text='Фильтры', width=7, height=1 )
+filters.place( x=670, y=30 )
+
+#надпись "По дате торгов"
+date_torg = Label( root, text='По дате торгов', width=14, height=1 )
+date_torg.place( x=612, y=60 )
+
+#надпись "ОТ"
+ot = Label( root, text='ОТ', width=2, height=1 )
+ot.place( x=620, y=90 )
+
+#текстовое поле для ввода левой границы даты
+date_torg_from = Text( root, width=6, height=1 )
+date_torg_from.place( x=642, y=90 )
+
+#надпись "ДО"
+do = Label( root, text='ДО', width=2, height=1 )
+do.place( x=700, y=90 )
+
+#текстовое поле для ввода правой границы даты
+date_torg_to = Text( root, width=6, height=1 )
+date_torg_to.place( x=722, y=90 )
+
+#надпись "По имени фьючерса"
+name = Label( root, text='По имени фьючерса', width=17, height=1 )
+name.place( x=617, y=120 )
+
+#список с выбором имени фьючерса
+name_sel = StringVar(root)
+name_sel.set('-Выберите фьючерс-')
+name = OptionMenu( root, name_sel, '-Выберите фьючерс-', 'тест1', 'тест2', 'тест3' )
+name.place( x=620, y=150 )
+
+
+
+height = 5
+width = 5
+cells = {}
+for i in range(height): #Rows
+    for j in range(width): #Columns
+        b = Entry(root, text="")
+        b.grid(row=i, column=j)
+        cells[(i,j)] = b
+
+
+
+#кнопка "Восстановить таблицу"
+restore = Button( root, text='Восстановить таблицу', width=20, height=1 )
+restore.place( x=800, y=60 )
+
+#кнопка "Новая запись"
+newentry = Button( root, text='Новая запись', width=20, height=1 )
+newentry.place( x=800, y=90 )
+
+#кнопка "Удалить запись"
+delentry = Button( root, text='Удалить запись', width=20, height=1 )
+delentry.place( x=800, y=120 )
+
+#кнопка "Редактировать запись"
+editentry = Button( root, text='Редактировать запись', width=20, height=1 )
+editentry.place( x=800, y=150 )
+
+db_connect.close()                               
+root.mainloop()              #удержание окна
