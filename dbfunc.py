b1a17a02d22c2a3ba62efb70fc16dc73947f8853
V.Shcherbakov <vitya.shcherbakov@gmail.com>
2020-10-09 20:39:36 +0400

    Update

--- dbfunc.py
+++ dbfunc.py
@@ -1,10 +1,11 @@
-import os, csv, mysql.connector, tkinter.font
+import os, csv, mysql.connector
 from tkinter import *
 from tkinter.ttk import *
 from tkinter.scrolledtext import *
 
-#функция, загружающая таблицу при старте и при применении фильтра
-def load_and_filter(root, l_date, r_date, name, l_quot, r_quot):
+#функция, делающая запрос по таблице
+def load_and_filter(username, username_password, l_date, r_date,
+                    name, l_quot, r_quot, ord_column, ord_ascdesc):
     #удаление файла с таблицей, если он существует
     if os.path.exists('C:\\temp\\out.csv'):
         os.remove('C:\\temp\\out.csv')
@@ -17,30 +18,25 @@ def load_and_filter(root, l_date, r_date, name, l_quot, r_quot):
     #в тот же файл добавить строку tmpdir = "C:/temp/" и создать на диске C папку temp
     db = mysql.connector.connect( host='localhost',
                                   database='fut_cen_bum',
-                                  user='root',
-                                  password='0000' )        #добавить считывание из конфига и редактирование этих файлов
+                                  user=username,
+                                  password=username_password )    #добавить считывание из конфига и редактирование этих файлов
     dbcursor = db.cursor()
-    dbcursor.callproc( 'filtr', (l_date, r_date, name, l_quot, r_quot) )
+    dbcursor.callproc( 'filtr', (l_date, r_date, name, l_quot, r_quot, ord_column, ord_ascdesc) )
     dbcursor.close()
     db.close()
 
-    #открытие файла с таблицей
     with open( 'C:\\temp\\out.csv', 'r' ) as tablefile:
         table = csv.DictReader( tablefile, fieldnames=('Дата торг.', 'Код фьюч.', 'Дата погаш.', 'Тек.цена',
                                                        'Мин.цена', 'Макс.цена', 'Число прод.') )
         table_format = ''
-        row_id=0
+        row_id = 0
         for row in table:
             row_id=row_id+1
             table_format = table_format + '{:>4}'.format(row_id) + ' │' + '{: <12}'.format(row['Дата торг.']+' │') \
                          + '{: <12}'.format(row['Код фьюч.']) + ' │' + '{: <12}'.format(row['Дата погаш.']+' │') \
                          + '{: <9}'.format(row['Тек.цена']) + '│' + '{: <9}'.format(row['Мин.цена']) +'│' \
                          + '{: <11}'.format(row['Макс.цена']) + '│' + '{: <11}'.format(row['Число прод.']) + '\n'
-    #отображение таблицы
-    table_out = ScrolledText( root, width=88, height=25, font=('Courier New', 9, 'underline italic bold') )
-    table_out.insert( 0.1, table_format )
-    table_out.place( x=10, y=30 )
-    table_out.configure(state='disabled')
+    return table_format
 
 #проверка на правильность введенного формата данных
 def date_check(date):
@@ -63,32 +59,159 @@ def date_check(date):
     else: return False
     return True
 
+#проверка на хронологию дат(левая меньше правой)
+def chrono_check(l_date, r_date):
+    if ( date_check(l_date)==False or date_check(r_date)==False or  
+         int(l_date[:4])>int(r_date[:4]) or int(l_date[:4])==int(r_date[:4]) and
+         int(l_date[5:7])>int(r_date[5:7]) or int(l_date[5:7])==int(r_date[5:7]) and
+         int(l_date[8:10])>int(r_date[8:10]) ):
+        return False
+    return True
+
 #проверка quotation
 def quot_check(quot):
     flag_dot = 0
+    if quot=='': return False
     for symb in quot:                         #поиск недопустимых символов
         if symb.isnumeric()==True: pass
         elif symb=='.' or symb==',':
-            flag_dot = flag_dot+1             #обнаружена одна запятая или точка, больше их уже быть не может
+            flag_dot += 1                     #обнаружена одна запятая или точка, больше их уже быть не может
         elif flag_dot>1:                      #если найдена еще одна точка (такого быть не может)
             return False
         else: return False
     quot = quot.replace(',','.')
-    if float(quot)<0:
-        return False
-    if quot[0]=='0' and quot[1]!='.' and len(quot)>1:          #если число начинается на 0, но не 0.
-        return False
+    if float(quot)<0: return False
+    if quot[0]=='0' and quot[1]!='.' and len(quot)>1: return False  #если число начинается на 0, но не 0.
     return True
+
+#проверка номера записи для удаления
+def del_check(delete, max_entry):
+    if delete=='': return False
+    for symb in delete:                       #поиск недопустимых символов
+        if symb.isnumeric()==False or symb=='':
+            if symb==' ': pass
+            else: return False
+    del_list = [int(to_del) for to_del in delete.split(' ')]
+    print(del_list)
+    for dlist in del_list:
+        if dlist<=0: return False
+        if dlist>max_entry: return False
+        if dlist==0: return False
+    return True
+
+#проверка номера записи для изменения
+def edit_check(edit, max_entry):
+    if edit=='': return False
+    for symb in edit:                         #поиск недопустимых символов
+        if symb.isnumeric()==False: return False
+    if int(edit)<=0: return False
+    if edit[0]=='0': return False             #если введено число, начинающееся 0.
+    if int(edit)>max_entry: return False
+    return True
+
+#подсчет строк
+def count_rows(text):
+    row_num = text.count('\n')-1              #+1 пустая строка
+    return row_num
+
+#функция, открывающая окно с изменением/добавлением записи
+def edit_new_window(root, edit_or_new_str):
+    
+    #добавить проверку на существование еще одного окна
+    
+    edit_new_wnd = Toplevel(root)
+    edit_new_wnd.geometry('590x140+420+250')
+    edit_new_wnd.title( edit_or_new_str )
+    edit_new_wnd.minsize(590,140)
+    edit_new_wnd.maxsize(590,140)
+    edit_new_msg = Label( edit_new_wnd, text=edit_or_new_str, font=('TkDefault', 12, 'bold'), anchor='center', width=40 )
+    edit_new_msg.place( relx=0.5, rely=0.1, anchor=CENTER )
+    
+    #отображение заголовков колонок таблицы
+    table_header_en = Text( edit_new_wnd, width=80, height=1, font=('Courier New', 9, 'bold') )
+    table_header_en.insert(0.1,' Дата торг.    Код фьюч.   Дата погаш. Тек.цена  Мин.цена  Макс.цена Число прод.')
+    table_header_en.place( x=10, y=30 )
+    table_header_en.configure(state='disabled')
+
+    #ячейки для ввода
+    torg_date_en = Entry( edit_new_wnd, width=10, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
+    torg_date_en.place( x=10, y=50 )
+    name_en = Entry( edit_new_wnd, width=12, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
+    name_en.place( x=95, y=50 )
+    day_end_en = Entry( edit_new_wnd, width=10, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
+    day_end_en.place( x=196, y=50 )
+    quot_en = Entry( edit_new_wnd, width=8, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
+    quot_en.place( x=281, y=50 )
+    min_quot_en = Entry( edit_new_wnd, width=8, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
+    min_quot_en.place( x=350, y=50 )
+    max_quot_en = Entry( edit_new_wnd, width=8, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
+    max_quot_en.place( x=419, y=50 )
+    num_contr_en = Entry( edit_new_wnd, width=10, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
+    num_contr_en.place( x=488, y=50 )
+
+    #пояснения к ячейкам
+    date_format_torg = Label( edit_new_wnd, text='yyyy-mm-dd', font=('TkDefaultFont', 9), width=10 )
+    date_format_torg.place( x=18, y=73 )
+    date_format_end = Label( edit_new_wnd, text='yyyy-mm-dd', font=('TkDefaultFont', 9), width=10 )
+    date_format_end.place( x=204, y=73 )
+    name_format = Label( edit_new_wnd, text='12345-6789', font=('TkDefaultFont', 9), width=10 )
+    name_format.place( x=110, y=73 )
+    
+    #кнопка "Применить"
+    def edit_new_set_on(self):
+        torg_date_entry = torg_date_en.get()
+        name_entry = name_en.get()
+        day_end_entry = day_end_en.get()
+        quot_entry = quot_en.get()
+        min_quot_entry = min_quot_en.get()
+        max_quot_entry = max_quot_en.get()
+        num_contr_entry = num_contr_en.get()
+        if ( date_check(torg_date_entry) and len(name_entry)==11 and name_entry[5]=='-' and
+             date_check(day_end_entry) and quot_check(quot_entry) and
+             quot_check(min_quot_entry) and quot_check(max_quot_entry) and 
+             chrono_check(torg_date_entry, day_end_entry) ):
+            name_numeric_part1 = name_entry[:5]
+            name_numeric_part2 = name_entry[6:]
+            name_numeric_part = name_numeric_part1 + name_numeric_part2
+            for symb1 in name_numeric_part:
+                if symb1.isnumeric()==False:
+                    error_window(root, 'Неправильный формат записи!')
+                else:
+                    for symb2 in num_contr_entry:
+                        if symb2.isnumeric()==False:
+                            error_window(root, 'Неправильный формат записи!')
+                        else:
+                            if int(num_contr_entry)<0:
+                                error_window(root, 'Неправильный формат записи!')
+                            else:
+                                error_window(root, 'Успех')
+        else:
+            error_window(root, 'Неправильный формат записи!')
+               
+    edit_new_set = Button( edit_new_wnd, text='Применить', width=12 )
+    edit_new_set.place( x=206, y=100 )
+    edit_new_set.bind( '<Button-1>', edit_new_set_on )
+
+    #кнопка "Отмена"
+    def edit_new_cancel_on(self):
+        edit_new_wnd.destroy()
+    edit_new_cancel = Button( edit_new_wnd, text='Отмена', width=12 )
+    edit_new_cancel.place( x=310, y=100 )
+    edit_new_cancel.bind( '<Button-1>', edit_new_cancel_on )
+    
+    edit_new_wnd.mainloop()
 
 #функция, открывающая окно-уведомление об ошибке
 def error_window(root, err_str):
+    
     #добавить проверку на существование еще одного окна
+    
     error_wnd = Toplevel(root)
     error_wnd.geometry('320x100+420+250')
     error_wnd.title('Ошибка')
     error_wnd.minsize(320,100)
     error_wnd.maxsize(320,100)
-    error_msg = Label( error_wnd, text=err_str, anchor='center', width=40 )
+    error_msg = Label( error_wnd, text=err_str, anchor='center', width=60 )
     error_msg.place( relx=0.5, rely=0.33, anchor=CENTER )
     
     #кнопка ОК
--- main.py
+++ main.py
@@ -1,81 +1,125 @@
+import os, sys
 from dbfunc import *
 
-#путь к этому скрипту
-#PATH = os.path.dirname(sys.argv[0])
-#PATH.replace('\\\\', '\\')
+#считывание логина и пароля из конфига
+with open( os.path.dirname(sys.argv[0])+'\\config.txt', 'r' ) as user:
+    line_count = 1
+    for line in user:
+        if line_count==1:
+            username = line
+        else:
+            username_password = line
+        line_count = line_count + 1
 
 #создание и настройка окна
 root=Tk()                               
-root.geometry('1000x445')               
+root.geometry('1080x445')               
 root.title('Фьючерсы')
 
 #загрузка таблицы при старте 
-load_and_filter(root, '1996-02-05', '1996-10-30', 'NOT_SET', 78.4, 85)
+table_format = load_and_filter(username, username_password, '1996-02-05',
+                               '1996-10-30', '-не выбрано-', 78.4, 85, 'torg_date', 'по возр.')
 
 #НАДПИСИ
 #надпись "Фильтры"
 filters_label = Label( root, text='Фильтрация', font=('TkDefaultFont', 12, 'bold'), width=13 )
-filters_label.place( x=685, y=10 )
+filters_label.place( x=695, y=10 )
 #надпись "Диапазон по дате торгов"
-date_torg_label = Label( root, text='Диапазон по дате торгов', font=('TkDefaultFont', 9, 'underline'), width=24 )
-date_torg_label.place( x=655, y=40 )
-#date_torg_label.configure(underline=True)
+torg_date_label = Label( root, text='Диапазон по дате торгов', font=('TkDefaultFont', 9, 'underline'), width=24 )
+torg_date_label.place( x=660, y=40 )
 #надпись "По имени фьючерса"
 name_label = Label( root, text='По имени фьючерса', font=('TkDefaultFont', 9, 'underline'), width=24 )
-name_label.place( x=655, y=92 )
+name_label.place( x=660, y=92 )
 #надпись "Диапазон по котировке"
-quot_label = Label( root, text='Диапазон по котировке', font=('TkDefaultFont', 9, 'underline'), width=24 )
-quot_label.place( x=655, y=142 )
+quot_label = Label( root, text='Диапазон по тек. цене', font=('TkDefaultFont', 9, 'underline'), width=24 )
+quot_label.place( x=660, y=144 )
+#надпись "Упорядочить по"
+order_label = Label( root, text='Упорядочить по', font=('TkDefaultFont', 9, 'underline'), width=24 )
+order_label.place( x=660, y=196 )
+#надпись "Изменение"
+edit_label = Label( root, text='Изменение', font=('TkDefaultFont', 12, 'bold'), width=13 )
+edit_label.place( x=910, y=10 )
+#надпись "Удаление записей"
+delete_entry_label = Label( root, text='Удалить записи', font=('TkDefaultFont', 9, 'underline'), width=24 )
+delete_entry_label.place( x=870, y=40 )
+#надпись "Изменение записи"
+edit_entry_label = Label( root, text='Изменить запись', font=('TkDefaultFont', 9, 'underline'), width=24 )
+edit_entry_label.place( x=870, y=144 )
+#надпись с примером формата удаления
+delete_format_label = Label( root, text='номера записей через пробел', font=('TkDefaultFont', 9), width=30 )
+delete_format_label.place( x=868, y=84 )
+#надпись "номер записи"
+edit_id_label = Label( root, text='№ записи', font=('TkDefaultFont', 9), width=30 )
+edit_id_label.place( x=984, y=166 )
 
+#табличная форма
+table_out = ScrolledText( root, width=88, height=25, font=('Courier New', 9, 'underline italic bold') )
+table_out.insert( 0.1, table_format )
+table_out.place( x=10, y=30 )
+table_out.configure(state='disabled')
+
+#пользовательский стиль записей и выпадающих списков
 entry_style = Style()
 entry_style.configure('TEntry', foreground='blue')
+combo_style = Style()
+combo_style.configure('TCombobox', foreground='blue')
 
 #текстовое поле для ввода левой границы даты
-date_torg_from = Entry( root, width=10, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
-date_torg_from.place( x=655, y=62 )
-#date_torg_from.insert(0, 'yyyy-mm-dd')           #поставить раннюю дату
-date_torg_from.insert(0, '1996-02-05') 
+torg_date_from = Entry( root, width=10, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
+torg_date_from.place( x=660, y=62 )
+#torg_date_from.insert(0, 'yyyy-mm-dd')           #поставить раннюю дату
+torg_date_from.insert(0, '1996-02-05') 
 
 #текстовое поле для ввода правой границы даты
-date_torg_to = Entry( root, width=10, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
-date_torg_to.place( x=745, y=62 )
-#date_torg_to.insert(0, 'yyyy-mm-dd')            #поставить самую позднюю дату.
-date_torg_to.insert(0, '1996-10-30')
+torg_date_to = Entry( root, width=10, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
+torg_date_to.place( x=750, y=62 )
+#torg_date_to.insert(0, 'yyyy-mm-dd')            #поставить самую позднюю дату.
+torg_date_to.insert(0, '1996-10-30')
 
 #список с выбором имени фьючерса
 name_menu = Combobox( root )
-name_menu['values'] = ('-Выберите фьючерс-', 'тест1', 'тест2', 'тест3')
+name_menu['values'] = ('-не выбрано-', 'тест1', 'тест2', 'тест3')
 name_menu.current(0)
-name_menu.place( x=655, y=114 )
+name_menu.place( x=660, y=114, width=178 )
 
 #текстовое поле для ввода левой границы котировки
 quot_from = Entry( root, width=10, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
-quot_from.place( x=655, y=164 )
+quot_from.place( x=660, y=166 )
 #quot_from.insert(0, '0')                        #поставить минимальное число
 quot_from.insert(0, '78.4')    
 
 #текстовое поле для ввода правой границы котировки
 quot_to = Entry( root, width=10, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
-quot_to.place( x=745, y=164 )
+quot_to.place( x=750, y=166 )
 #quot_to.insert(0, '0')                          #поставить максимальное число
 quot_to.insert(0, '85')
+
+#список с выбором столбцов
+order_column_menu = Combobox( root, style='combo_style.TCombobox', width=11 )
+order_column_menu['values'] = ('Дата торг.', 'Код фьюч.', 'Дата погаш.', 'Тек.цена', 'Мин.цена', 'Макс.цена', 'Число прод.')
+order_column_menu.current(0)
+order_column_menu.place( x=660, y=218 )
+
+#список с выбором типа сортировки
+order_ascdesc_menu = Combobox( root, style='combo_style.TCombobox', width=11 )
+order_ascdesc_menu['values'] = ('по возр.', 'по убыв.')
+order_ascdesc_menu.current(0)
+order_ascdesc_menu.place( x=750, y=218 )
 
 #кнопка "Применить фильтры"
 def append_on(self):               #записывает введенные значения в переменны
     
     #проверка даты
-    l_date = date_torg_from.get()
-    r_date = date_torg_to.get()
-    if date_check(l_date)==True and date_check(r_date)==True: 
-        if int(l_date[:4])>int(r_date[:4]) or int(l_date[:4])==int(r_date[:4]) and int(l_date[5:7])>int(r_date[5:7]):
-            error_window(root, 'Неправильно введена дата!')
-        elif int(l_date[5:7])==int(r_date[5:7]) and int(l_date[8:10])>int(r_date[8:10]):   #разбито на 2 условия для удобства
-            error_window(root, 'Неправильно введена дата!')
-    else:
+    l_date = torg_date_from.get()
+    r_date = torg_date_to.get()
+    if chrono_check(l_date, r_date)==False:
         error_window(root, 'Неправильно введена дата!')
 
     #доделать с выбором имени фьючерса
-    name = '22020-1503'
+    if name_menu.get()=='-не выбрано-':
+        name = '-не выбрано-'
+    else:
+        name = '22020-1503'
          
     #проверка котировок   
     l_quot = quot_from.get()
@@ -87,27 +131,35 @@ def append_on(self):               #записывает введенные зн
         r_quot = r_quot.replace(',','.')
         if float(l_quot)>float(r_quot):
             error_window(root, 'Неправильно введена котировка!')
-            
-    #загрузка таблицы
-    load_and_filter(root, l_date, r_date, name, l_quot, r_quot)
+
+    #упорядочивание
+    ord_column = order_column_menu.get()
+    ord_ascdesc = order_ascdesc_menu.get()
+
+    #отображение таблицы
+    table_out.configure(state='normal')
+    table_out.delete( 0.1, END )
+    table_out.insert( 0.1, load_and_filter(username, username_password, l_date,
+                                           r_date, name, l_quot, r_quot, ord_column, ord_ascdesc) )
+    table_out.configure(state='disabled')
     
 append = Button( root, text='Применить фильтры', width=22 )
-append.place( x=655, y=270 )
+append.place( x=680, y=252 )
 append.bind( '<Button-1>', append_on )
 
 #кнопка "Сбросить фильтры"
 def clear_on(self):
-    date_torg_from.delete(0, END)
-    date_torg_from.insert(0, 'yyyy-mm-dd')      #поставить раннюю дату
-    date_torg_to.delete(0, END)
-    date_torg_to.insert(0, 'yyyy-mm-dd')        #поставить самую позднюю дату
+    torg_date_from.delete(0, END)
+    torg_date_from.insert(0, 'yyyy-mm-dd')      #поставить раннюю дату
+    torg_date_to.delete(0, END)
+    torg_date_to.insert(0, 'yyyy-mm-dd')        #поставить самую позднюю дату
     quot_from.delete(0, END)
     quot_from.insert(0, '0')                    #поставить минимальное число
     quot_to.delete(0, END)
     quot_to.insert(0, '0')                      #поставить максимальное число
 
 clear = Button( root, text='Сбросить фильтры', width=22 )
-clear.place( x=655, y=300 )
+clear.place( x=680, y=283 )
 clear.bind( '<Button-1>', clear_on )
 
 #отображение заголовков колонок таблицы
@@ -116,22 +168,58 @@ table_header.insert(0.1,'  №  │Дата торг. │Код фьюч.    │
 table_header.place( x=10, y=10 )
 table_header.configure(state='disabled')
 
+#поле для ввода записей для удаления
+delete_entry = Entry( root, width=21, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
+delete_entry.place( x=870, y=62 )
+
+#кнопка "Удалить"
+def delete_on(self):
+    to_delete = delete_entry.get()
+    max_row = count_rows( table_out.get(0.1, END) )
+    if del_check( to_delete, max_row ): pass
+        
+    else:
+        error_window(root, 'Неправильно указаны номера для удаления!')
+
+delete = Button( root, text='Удалить', width=22 )
+delete.place( x=885, y=111 )
+delete.bind( '<Button-1>', delete_on )
+
+#текстовое поле для ввода номера изменяемой записи
+edit_entry = Entry( root, width=7, style='entry_style.TEntry', font=('Courier New', 10, 'bold') )
+edit_entry.place( x=983, y=144 )
+
+#кнопка "Изменить"
+def edit_on(self):
+    to_edit = edit_entry.get()
+    max_row = count_rows( table_out.get(0.1, END) )           
+    if edit_check( to_edit, max_row ):        
+        edit_check_label = Label( root, 
+                                  text=max_row,
+                                  font=('TkDefaultFont', 9),
+                                  width=30 )
+        edit_check_label.place( x=984, y=270 )
+        edit_new_window(root, 'Изменение записи '+to_edit)
+    else:
+        error_window(root, 'Неправильно введен номер записи!')
+
+edit = Button( root, text='Изменить', width=22 )
+edit.place( x=885, y=194 )
+edit.bind( '<Button-1>', edit_on )
+
 #кнопка "Новая запись"
-newentry = Button( root, text='Новая запись', width=22 )
-newentry.place( x=840, y=60 )
+def new_on(self):
+    edit_new_window(root, 'Новая запись')
 
-#кнопка "Удалить запись"
-delentry = Button( root, text='Удалить запись', width=22 )
-delentry.place( x=840, y=90 )
-
-#кнопка "Редактировать запись"
-editentry = Button( root, text='Редактировать запись', width=22 )
-editentry.place( x=840, y=120 )
+new = Button( root, text='Новая запись', width=22 )
+new.place( x=885, y=224 )
+new.bind( '<Button-1>', new_on )
 
 #обработчик закрытия окна
 def root_close():
     childs = root.place_slaves()
     for obj in childs:
         obj.destroy()
+    root.destroy()
 root.protocol("WM_DELETE_WINDOW", root_close)  #обработка закрытия окна
 root.mainloop()                                #удержание окна
